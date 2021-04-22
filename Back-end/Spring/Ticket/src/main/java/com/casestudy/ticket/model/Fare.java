71f50087c9f8ef7af6b1ff24355b979f36920e00
Yashasvi Pamu <yashasvistark028@gmail.com>
2021-04-22 19:32:24 +0530

    Add files via upload

---
+++ Back-end/Spring/Ticket/src/main/java/com/casestudy/ticket/model/Fare.java
@@ -0,0 +1,18 @@
+package com.casestudy.ticket.model;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class Fare {
+	@Id
+	private int farenum;
+	private String bookingid;
+	private double bookingfare;
+}
