181db5de54dfe40753445bcbabf37690430c1884
Yashasvi Pamu <yashasvistark028@gmail.com>
2021-04-22 19:28:00 +0530

    Delete Ticket directory

--- Ticket/src/main/java/com/casestudy/ticket/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.casestudy.ticket.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.casestudy.ticket.model.Ticket;
-
-public interface TicketRepository extends MongoRepository<Ticket, Integer>{
-
-}
